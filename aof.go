54e8b6f8cc4094bd27f4fb55a1e25b3a7871196a
Rishikesh Suvarna <rishikeshsuvarna@gmail.com>
2024-09-03 19:21:14 +0100

    feat: Created a AOF handler to handle data persistence using a file

---
+++ aof.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"sync"
+	"time"
+)
+
+type Aof struct {
+	file *os.File
+	rd   *bufio.Reader
+	mu   sync.Mutex
+}
+
+// * INITIALISING AOF
+func NewAof(path string) (*Aof, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	aof := &Aof{
+		file: f,
+		rd:   bufio.NewReader(f),
+	}
+
+	// * GO ROUTINE TO SYNC FILE EVERY SECOND
+	go func() {
+		for {
+			aof.mu.Lock()
+
+			aof.file.Sync()
+
+			aof.mu.Unlock()
+
+			time.Sleep(time.Second)
+		}
+	}()
+
+	return aof, nil
+}
+
+// * CLOSING FILE READER
+func (aof *Aof) Close() error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	return aof.file.Close()
+}
